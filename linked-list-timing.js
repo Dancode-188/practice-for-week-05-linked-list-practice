b2e53e322568fdd4a03dfdc1c86fbe21f34ddbc4
Dancode-188 <danbitengo@gmail.com>
2024-09-25 07:24:41 +0300

    Add timing test for addToHead and addToTail methods for both Singly Linked List and Doubly Linked List

--- linked-list-timing.js
+++ linked-list-timing.js
@@ -7,3 +7,35 @@ Construct a timing test to verify the time complexities of `addToHead` and
 */
 
 // Your code here
+
+function testTiming() {
+    const singlyLinkedList = new LinkedList();
+    const doublyLinkedList = new DoublyLinkedList();
+    const times = 10000;
+
+    console.time('singlyLinkedList.addToHead');
+    for (let i = 0; i < times; i++) {
+        singlyLinkedList.addToHead(i);
+    }
+    console.timeEnd('singlyLinkedList.addToHead');
+
+    console.time('singlyLinkedList.addToTail');
+    for (let i = 0; i < times; i++) {
+        singlyLinkedList.addToTail(i);
+    }
+    console.timeEnd('singlyLinkedList.addToTail');
+
+    console.time('doublyLinkedList.addToHead');
+    for (let i = 0; i < times; i++) {
+        doublyLinkedList.addToHead(i);
+    }
+    console.timeEnd('doublyLinkedList.addToHead');
+
+    console.time('doublyLinkedList.addToTail');
+    for (let i = 0; i < times; i++) {
+        doublyLinkedList.addToTail(i);
+    }
+    console.timeEnd('doublyLinkedList.addToTail');
+}
+
+testTiming();
